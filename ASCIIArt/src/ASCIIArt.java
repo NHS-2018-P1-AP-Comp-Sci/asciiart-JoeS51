a7b18f878d7a96501cfbc80344832e9028f01c70
s-sluisj <s-sluisj@53S500036646.it.bsd405.org>
2018-09-19 08:59:23 -0700

    This is my ascii art project. It is pacman eating cherries.

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -15,3 +15,89 @@
  */
 
 // YOUR CODE GOES HERE :)
+public class ASCIIArt {
+	public static void main(String[]args) {
+		makeTop();
+		makeTop2();
+		makeTop3();
+		makeTop4();
+		makeTop5();
+		makeTop6();
+		makeTop7();
+		System.out.println("                       /\\                   /\\");
+		makeTop8();
+		System.out.println("                          |                     |");
+		makeTop9();
+		System.out.println("                             |______               |_____");
+		makeTop10();
+		System.out.println("                                (\"\"\"\"\"\")              (\"\"\"\"\"\")");
+		makeTop11();
+		System.out.println(" (\"\"\"\"\"\") 183&#$)(&)&% (\"\"\"\"\"\") !^)@*&($^)(^#@#%*$$%&#&%@$%@!%#$#&^#");
+		makeTop12();
+		System.out.println("                                      (\"\"\"\"\"\")              (\"\"\"\"\"\")");
+		makeTop111();
+		makeTop100();
+		makeTop99();
+		makeTop88();
+		makeTop77();
+		makeTop6();
+		makeTop5();
+		makeTop4();
+		makeTop3();
+		makeTop2();
+		makeTop();
+		System.out.println("");
+		System.out.println("This is pacman about to eat some cherry and the random series of symbols are just meant to add a cool aesthetic.");
+	}
+	public static void makeTop() {
+		System.out.println("                                         @@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop2() {
+		System.out.println("                               @@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop3() {
+		System.out.println("                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop4() {
+		System.out.println("                   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop5() {
+		System.out.println("                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop6() {
+		System.out.println("            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop7() {
+		System.out.print("         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop8() {
+		System.out.print("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop9() {
+		System.out.print("     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop10() {
+		System.out.print("    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+	public static void makeTop11() {
+		System.out.print("  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        @#$%@^$#%^@$#^%*@%#(*^#)(&");                        
+	}
+	public static void makeTop12() {
+		System.out.print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+	}
+		public static void makeTop77() {
+			System.out.println("         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		}
+		public static void makeTop88() {
+			System.out.println("      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		}
+		public static void makeTop99() {
+			System.out.println("     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		}
+		public static void makeTop100() {
+			System.out.println("    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		}
+		public static void makeTop111() {
+			System.out.println("  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@        ");   
+	}
+}
